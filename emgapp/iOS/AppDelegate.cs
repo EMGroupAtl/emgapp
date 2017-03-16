b6f2d2043a37173105aff8a70276146779342408
vivianaranha <vivianaranha@gmail.com>
2017-03-16 17:36:38 -0400

    Adding Analytics and Crash

--- emgapp/iOS/AppDelegate.cs
+++ emgapp/iOS/AppDelegate.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 using Foundation;
 using UIKit;
@@ -15,6 +18,9 @@ namespace emgapp.iOS
 			global::Xamarin.Forms.Forms.Init();
 
 			LoadApplication(new App());
+
+			MobileCenter.Start("3e2330ef-d7b4-46ec-9d4c-40be4eecc107",
+				   typeof(Analytics), typeof(Crashes));
 
 			return base.FinishedLaunching(app, options);
 		}
